69b3fe27c264991db87ccef52b1bc4d4243c5cee
kazimehedihasan1 <mehedihasanarabi00@gmail.com>
2024-08-28 01:04:29 +0600

    car-type section done

--- src/Components/pages/Home/CarType.jsx
+++ src/Components/pages/Home/CarType.jsx
@@ -9,46 +9,46 @@ import img8 from "../../../assets/truck.svg";
 import img9 from "../../../assets/van.svg";
 const CarType = () => {
   return (
-    <div className="max-w-screen-xl mt-14 mx-auto">
+    <div className="max-w-screen-xl mt-28 mx-auto">
       <h1 className="font-primary font-bold text-center text-4xl">
         Browse by Type
       </h1>
-      <div className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-9 mt-16">
+      <div className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-9 mt-10">
         <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm w-28">
-          <img src={img1} alt="bmw" className="w-9 h-10" />
-          <p>BMW</p>
+          <img src={img1} alt="bmw" className="w-9 h-10 mx-auto" />
+          <p className="text-center">BMW</p>
         </div>
-        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm">
-          <img src={img2} alt="" />
-          <p>Convertible</p>
+        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm w-28">
+          <img src={img2} alt="" className="w-9 h-10 mx-auto"/>
+          <p className="text-center">Convertible</p>
         </div>
-        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm">
-          <img src={img3} alt="" />
-          <p>Coupe</p>
+        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm w-28">
+          <img src={img3} alt="coupe" className="w-9 h-10 mx-auto" />
+          <p className="text-center">Coupe</p>
         </div>
-        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm">
-          <img src={img4} alt="" />
-          <p>Electric</p>
+        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm w-28">
+          <img src={img4} alt="electric" className="w-9 h-10 mx-auto"/>
+          <p className="text-center">Electric</p>
         </div>
-        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm">
-          <img src={img5} alt="" />
-          <p>Hatchback</p>
+        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm w-28">
+          <img src={img5} alt="hatchback" className="w-9 h-10 mx-auto"/>
+          <p className="text-center">Hatchback</p>
         </div>
-        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm">
-          <img src={img6} alt="" />
-          <p>Hybrid</p>
+        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm w-28">
+          <img src={img6} alt="hybrid" className="w-9 h-10 mx-auto"/>
+          <p className="text-center">Hybrid</p>
         </div>
-        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm">
-          <img src={img7} alt="" />
-          <p>sedan</p>
+        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm w-28">
+          <img src={img7} alt="sedan" className="w-9 h-10 mx-auto"/>
+          <p className="text-center">sedan</p>
         </div>
-        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm">
-          <img src={img8} alt="" />
-          <p>Truck</p>
+        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm w-28" >
+          <img src={img8} alt="truck" className="w-9 h-10 mx-auto" />
+          <p className="text-center">Truck</p>
         </div>
-        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm">
-          <img src={img9} alt="" />
-          <p>Van</p>
+        <div className="border mx-auto px-6 rounded-lg py-3 shadow-sm w-28">
+          <img src={img9} alt="van" className="w-9 h-10 mx-auto"/>
+          <p className="text-center">Van</p>
         </div>
       </div>
     </div>
